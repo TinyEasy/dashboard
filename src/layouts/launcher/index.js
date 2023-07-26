2c31cb894fcf39875a5f9caff6e3257f0e46bd46
TinyEasy <contact@tinyeasy.co.nz>
2023-07-26 13:07:51 +1200

    Improved operating failsafe screen

--- src/layouts/launcher/index.js
+++ src/layouts/launcher/index.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import IFrameLayout from "examples/LayoutContainers/IFrameLayout";
+import PageLayout from "examples/LayoutContainers/PageLayout";
+import { Grid } from "@mui/material";
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
 import { UserAuth } from "context/AuthContext";
 
 function Launcher() {
@@ -37,35 +41,106 @@ function Launcher() {
   };
 
   // Check if the platform is compatible (Windows, Mac, or Linux)
-  const isCompatiblePlatform = ["Win32", "MacIntel", "Linux x86_64"].includes(navigator.platform);
+  const isCompatiblePlatform = [
+    "Win32",
+    "Win64",
+    "Win16",
+    "Windows",
+    "Macintosh",
+    "Mac",
+    "Linux",
+    "Linux i686",
+    "Linux x86_64",
+  ].includes(navigator.platform);
 
+  if (!isCompatiblePlatform) {
+    return (
+      <PageLayout background="white">
+        <Grid container>
+          {/* Left Screen */}
+          <Grid item xs={12}>
+            <SoftBox
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              textAlign="center"
+              height="100vh"
+            >
+              <SoftBox pt={2} pb={3} px={3}>
+                {/* Headline */}
+                <SoftBox mb={1}>
+                  <img
+                    src={loadingGif}
+                    alt="Animated Logo"
+                    style={{ width: "75%", height: "auto" }}
+                  />
+                </SoftBox>
+                <SoftBox mb={1}>
+                  <SoftTypography variant="body1">
+                    👋 Hey! Your operating system is not compatible. Switch to a desktop or laptop
+                    device running Windows, Mac, or Linux.  <br />
+                    <br />
+                    See you soon! 😄
+                  </SoftTypography>
+                </SoftBox>
+              </SoftBox>
+            </SoftBox>
+          </Grid>
+        </Grid>
+      </PageLayout>
+    );
+  }
+
+  if (error) {
+    return (
+      <PageLayout background="white">
+        <Grid container>
+          {/* Left Screen */}
+          <Grid item xs={12}>
+            <SoftBox
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              textAlign="center"
+              height="100vh"
+            >
+              <SoftBox pt={2} pb={3} px={3}>
+                {/* Headline */}
+                <SoftBox mb={1}>
+                  <img
+                    src={loadingGif}
+                    alt="Animated Logo"
+                    style={{ width: "75%", height: "auto" }}
+                  />
+                </SoftBox>
+                <SoftBox mb={1}>
+                  <SoftTypography variant="body1">
+                    👋 Hey! Something went wrong. Please close the tab and open the 3D Tiny House
+                    Designer again.😄
+                  </SoftTypography>
+                </SoftBox>
+              </SoftBox>
+            </SoftBox>
+          </Grid>
+        </Grid>
+      </PageLayout>
+    );
+  }
   return (
-    <div style={{ overflow: "hidden" }}>
-      {!isCompatiblePlatform ? ( // Show error message if the platform is not compatible
-        <div style={{ textAlign: "center", padding: "20px" }}>
-          <p>Operating system not compatible. Switch to a device running Windows, Mac, or Linux.</p>
-        </div>
-      ) : error ? ( // Show error message if there's an error
-        <div style={{ textAlign: "center", padding: "20px" }}>
-          <p>
-            Something went wrong. Please close the tab and open the 3D Tiny House Designer again.
-          </p>
-        </div>
-      ) : (
-        <IFrameLayout background="white" style={{ border: "none", margin: "0", padding: "0" }}>
-          <div style={{ overflow: "hidden" }}>
-            <iframe
-              src={url}
-              width={iframeWidth}
-              height={iframeHeight - 10}
-              title="3D Tiny House Designer"
-              style={{ border: "none", margin: "0", padding: "0" }}
-              onError={handleIframeError} // Attach the error event handler
-            ></iframe>
-          </div>
-        </IFrameLayout>
-      )}
-    </div>
+    <IFrameLayout background="white" style={{ border: "none", margin: "0", padding: "0" }}>
+      <div style={{ overflow: "hidden" }}>
+        <iframe
+          src={url}
+          width={iframeWidth}
+          height={iframeHeight - 10}
+          title="3D Tiny House Designer"
+          style={{ border: "none", margin: "0", padding: "0" }}
+          onError={handleIframeError} // Attach the error event handler
+        ></iframe>
+      </div>
+    </IFrameLayout>
   );
 }
 
